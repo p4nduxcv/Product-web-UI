543b960ee2a519cb63fc57de104433024a15d89c
pandula <pandula95@gmail.com>
2020-11-03 21:41:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Router } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/pages/Navbar";
+import { BrowserRouter as Router1, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router1>
+      <Navbar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </Router1>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Navbar.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { MdFingerprint } from "react-icons/md";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "../../Button";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const ClickHandler = () => {
+    setClick(!click);
+    console.log(click);
+  };
+
+  const closeMobileMenu = () => {
+    setClick(!click);
+  };
+
+  const showButton = () => {
+    if (window.innerWidth < 960) {
+    }
+  };
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container.container">
+          <Link to="/" className="navbar-logo">
+            <MdFingerprint className="navbar-icon" />
+            PANDUXCV
+          </Link>
+
+          <div className="menu-icon" onClick={ClickHandler}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-items">
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-items">
+              <Link to="/" className="nav-links">
+                Services
+              </Link>
+            </li>
+            <li className="nav-items">
+              <Link to="/" className="nav-links">
+                Products
+              </Link>
+            </li>
+            <li className="nav-btn">
+              {button ? (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline"> SIGN UP</Button>
+                </Link>
+              ) : (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline" btnSize="btn--mobile">
+                    SIGN UP
+                  </Button>
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;

b48396cc0b55db7f9a93b484904499fe24b07451
pandula <pandula95@gmail.com>
2020-11-05 18:02:47 +0530

    Hover Option Done

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,15 +18,18 @@ export const Button = ({
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
+
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
   const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : COLOR[0];
+
   return (
-    <Button
+    <button
       className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
       onClick={onClick}
       type={type}
     >
       {children}
-    </Button>
+    </button>
   );
 };
